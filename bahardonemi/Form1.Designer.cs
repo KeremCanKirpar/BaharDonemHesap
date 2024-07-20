ee5fd0107e6f170919f30d9c17f7df18aaf40c88
Maverick <89163711+Maverivck@users.noreply.github.com>
2024-07-20 13:17:43 +0300

    Add project files.

---
+++ bahardonemi/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace bahardonemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtisim = new System.Windows.Forms.TextBox();
+            this.txtsoy = new System.Windows.Forms.TextBox();
+            this.txtvize = new System.Windows.Forms.TextBox();
+            this.txtfinal = new System.Windows.Forms.TextBox();
+            this.txtbit = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İsim";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(372, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "vize";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Soyisim";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(372, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "final";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(355, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Bitirme Ödevi";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(358, 214);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(320, 173);
+            this.listBox1.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "İşlem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtisim
+            // 
+            this.txtisim.Location = new System.Drawing.Point(162, 46);
+            this.txtisim.Name = "txtisim";
+            this.txtisim.Size = new System.Drawing.Size(100, 20);
+            this.txtisim.TabIndex = 16;
+            // 
+            // txtsoy
+            // 
+            this.txtsoy.Location = new System.Drawing.Point(162, 102);
+            this.txtsoy.Name = "txtsoy";
+            this.txtsoy.Size = new System.Drawing.Size(100, 20);
+            this.txtsoy.TabIndex = 17;
+            // 
+            // txtvize
+            // 
+            this.txtvize.Location = new System.Drawing.Point(482, 46);
+            this.txtvize.Name = "txtvize";
+            this.txtvize.Size = new System.Drawing.Size(100, 20);
+            this.txtvize.TabIndex = 18;
+            // 
+            // txtfinal
+            // 
+            this.txtfinal.Location = new System.Drawing.Point(482, 101);
+            this.txtfinal.Name = "txtfinal";
+            this.txtfinal.Size = new System.Drawing.Size(100, 20);
+            this.txtfinal.TabIndex = 19;
+            // 
+            // txtbit
+            // 
+            this.txtbit.Location = new System.Drawing.Point(482, 145);
+            this.txtbit.Name = "txtbit";
+            this.txtbit.Size = new System.Drawing.Size(100, 20);
+            this.txtbit.TabIndex = 20;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(162, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtbit);
+            this.Controls.Add(this.txtfinal);
+            this.Controls.Add(this.txtvize);
+            this.Controls.Add(this.txtsoy);
+            this.Controls.Add(this.txtisim);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtisim;
+        private System.Windows.Forms.TextBox txtsoy;
+        private System.Windows.Forms.TextBox txtvize;
+        private System.Windows.Forms.TextBox txtfinal;
+        private System.Windows.Forms.TextBox txtbit;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ bahardonemi/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bahardonemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string isim, soy;
+            double v1, f1, bit,ort;
+            isim = txtisim.Text;
+            soy = txtsoy.Text;
+            v1 = Convert.ToDouble(txtvize.Text);
+            f1 = Convert.ToDouble(txtfinal.Text);
+            bit = Convert.ToDouble(txtbit.Text);
+            ort = (v1 * 0.2) + (f1 * 0.3) + (bit * 0.5);
+            if(ort>=60)
+            {
+                listBox1.Items.Add(isim  + " " + soy + " =" + " " + v1 + " " + f1 + "  " + bit + " " + " = " + ort + " " + "Geçtiniz");
+            }
+            else
+            {
+                listBox1.Items.Add(isim  + " " + soy + " =" + " " + v1 + " " + f1 + "  " + bit + " "+" = "+ ort + " " + "Kaldınız");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtisim.Text = "";
+            txtsoy.Text = "";
+            txtvize.Text = "";
+            txtfinal.Text = "";
+            txtbit.Text = "";
+            
+
+        }
+    }
+}
